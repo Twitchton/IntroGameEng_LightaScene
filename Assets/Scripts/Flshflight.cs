1785e762d0d08b9d47e0ee4a9315d2638c1bc768
Matthieu Benedict <97464794+Twitchton@users.noreply.github.com>
2023-10-19 12:40:26 -0300

    Added Togglable Flashlight
    Added a flashlight using a light cookie and added a script to it to be toggleable using the 'F' key.

--- Assets/Scripts/Flshflight.cs
+++ Assets/Scripts/Flshflight.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Flshflight : MonoBehaviour
 {
+    private bool enabled;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            enabled = !enabled;
+            this.gameObject.GetComponent<Light>().enabled = enabled;
+        }
     }
 }
